1a3de6921fc1e74ab3bd7d361542e5f75bb3ded3
saiteki-kai <giuseppe.magazu@gmail.com>
2023-11-02 15:35:06 +0100

    fix: remove relative paths in tests

--- tests/test_database.rs
+++ tests/test_database.rs
@@ -4,23 +4,33 @@ use std::path::Path;
 use nika::core::models::dictionary::JMdict;
 use nika::core::repository::word_repository::WordRepository;
 
+static ROOT_DIR: &str = env!("CARGO_MANIFEST_DIR");
+
 async fn setup_repo() -> WordRepository {
-    let filepath = Path::new("data/test_words");
+    let db_folder = Path::new(ROOT_DIR).join("data");
+    let db_filepath = db_folder.join("test_words");
 
     // clear database
-    if filepath.is_dir() {
-        fs::remove_dir_all(filepath).expect("cannot delete directory");
+    if db_filepath.is_dir() {
+        fs::remove_dir_all(&db_filepath).expect("cannot delete directory");
     }
 
-    let repo = WordRepository::new("data", "test_words").await;
+    let repo = WordRepository::new(db_folder.to_str().unwrap(), "test_words").await;
 
     // load example data
-    let json_str = include_str!("./examples/words1.json");
-    let data: JMdict = serde_json::from_str(json_str).unwrap();
+    let examples_path = Path::new(ROOT_DIR).join("tests").join("examples");
+    let result = fs::read_to_string(examples_path.join("words1.json"));
 
-    // insert words in the database
-    for word in data.words.iter() {
-        repo.insert(word.clone()).await.unwrap();
+    match result {
+        Ok(json_str) => {
+            let data: JMdict = serde_json::from_str(&json_str).unwrap();
+
+            // insert words in the database
+            for word in data.words.iter() {
+                repo.insert(word.clone()).await.unwrap();
+            }
+        }
+        Err(error) => eprintln!("Error reading file: {}", error),
     }
 
     repo
