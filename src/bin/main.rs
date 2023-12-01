3a739358722eb8cf954ac4911dcc019d2fd42970
saiteki-kai <giuseppe.magazu@gmail.com>
2023-12-02 00:46:26 +0100

    style: format

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 
-use nika::cli::app::{init_word_repository, set_global_config, init_folders};
+use nika::cli::app::{init_folders, init_word_repository, set_global_config};
 use nika::cli::commands::{CommandHandler, DailyArgs, ProgressArgs, RandomArgs, SearchArgs};
 use nika::cli::config::load_config;
 
