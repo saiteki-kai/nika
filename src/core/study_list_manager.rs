245940157c71d378a960eea4aed17fbec475817b
saiteki-kai <giuseppe.magazu@gmail.com>
2024-03-09 18:13:52 +0100

    feat: add custom errors
    Move out the load and save functions from StudyListStats implementation

--- src/core/study_list_manager.rs
+++ src/core/study_list_manager.rs
@@ -1,9 +1,8 @@
-use anyhow::{Error, Result};
 use std::{fs, path::PathBuf};
 
 use crate::core::models::study_list::StudyConfig;
 
-use super::study_list_stats::StudyListStats;
+use super::study_list_stats::{Result, StudyListStats};
 
 pub struct StudyListManager {
     dirpath: PathBuf,
@@ -11,28 +10,27 @@ pub struct StudyListManager {
 }
 
 impl StudyListManager {
-    pub fn new(dirpath: PathBuf, stats_file: PathBuf) -> Self {
-        StudyListManager {
-            dirpath,
-            stats: StudyListStats::new(stats_file),
-        }
+    pub fn new(dirpath: PathBuf, stats_file: PathBuf) -> Result<Self> {
+        let stats = StudyListStats::new(stats_file)?;
+
+        Ok(StudyListManager { dirpath, stats })
     }
 
-    pub fn add(&mut self, name: &str, filepath: &PathBuf) -> Result<(), Error> {
+    pub fn add(&mut self, name: &str, filepath: &PathBuf) -> Result<()> {
         let new_filepath = self.dirpath.join(name);
         fs::copy(filepath, new_filepath)?;
 
         self.stats.update_stats(name, StudyConfig::default())
     }
 
-    pub fn remove(&mut self, name: &str) -> Result<(), Error> {
+    pub fn remove(&mut self, name: &str) -> Result<()> {
         let filepath = self.dirpath.join(name);
         fs::remove_file(filepath)?;
 
         self.stats.remove_stats(name)
     }
 
-    pub fn select_study_list(&mut self, name: &str) -> Result<(), Error> {
+    pub fn select_study_list(&mut self, name: &str) -> Result<()> {
         self.stats.select_list(name)
     }
 
--- src/core/study_list_stats.rs
+++ src/core/study_list_stats.rs
@@ -1,8 +1,40 @@
-use std::{fs, path::PathBuf};
+use std::{fs, io, path::PathBuf, result};
 
-use anyhow::{Error, Ok, Result};
+use serde_json::Error as SerdeError;
+use thiserror::Error;
 
 use crate::core::models::study_list::{StudyConfig, StudyListConfig};
+
+#[derive(Error, Debug)]
+pub enum StudyListError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Serde error: {0}")]
+    Serde(#[from] SerdeError),
+    #[error("List not found")]
+    ListNotFound,
+}
+
+pub type Result<T> = result::Result<T, StudyListError>;
+
+fn load(filepath: &PathBuf) -> Result<StudyListConfig> {
+    if !filepath.exists() {
+        let json = serde_json::to_string::<StudyListConfig>(&StudyListConfig::default())?;
+        fs::write(filepath, json)?;
+    }
+
+    let file = fs::read_to_string(filepath)?;
+    let config = serde_json::from_str::<StudyListConfig>(&file)?;
+
+    Ok(config)
+}
+
+fn save(filepath: &PathBuf, config: &StudyListConfig) -> Result<()> {
+    let json = serde_json::to_string::<StudyListConfig>(config)?;
+    fs::write(filepath, json)?;
+
+    Ok(())
+}
 
 pub struct StudyListStats {
     filepath: PathBuf,
@@ -10,10 +42,10 @@ pub struct StudyListStats {
 }
 
 impl StudyListStats {
-    pub fn new(filepath: PathBuf) -> Self {
-        let config = StudyListStats::load(&filepath).unwrap_or_default();
+    pub fn new(filepath: PathBuf) -> Result<Self> {
+        let config = load(&filepath)?;
 
-        StudyListStats { filepath, config }
+        Ok(StudyListStats { filepath, config })
     }
 
     pub fn get_list(&self, list_name: &str) -> Option<&StudyConfig> {
@@ -24,102 +56,88 @@ impl StudyListStats {
         self.config.lists.keys().cloned().collect()
     }
 
-    pub fn select_list(&mut self, list_name: &str) -> Result<(), Error> {
+    pub fn select_list(&mut self, list_name: &str) -> Result<()> {
         if self.config.lists.contains_key(list_name) {
             self.config.current = Some(list_name.to_string());
-            return self.save();
+
+            return save(&self.filepath, &self.config);
         }
 
-        Err(Error::msg("list not found"))
+        Err(StudyListError::ListNotFound)
     }
 
-    pub fn update_stats(&mut self, list_name: &str, config: StudyConfig) -> Result<(), Error> {
+    pub fn update_stats(&mut self, list_name: &str, config: StudyConfig) -> Result<()> {
         self.config.lists.insert(list_name.to_string(), config);
-        self.save()
+
+        save(&self.filepath, &self.config)
     }
 
-    pub fn remove_stats(&mut self, list_name: &str) -> Result<(), Error> {
-        if !self.config.lists.contains_key(list_name) {
-            return Err(Error::msg("Not found"));
-        }
+    pub fn remove_stats(&mut self, list_name: &str) -> Result<()> {
+        let result = self.config.lists.remove(list_name);
 
-        self.config.lists.remove(list_name);
+        if result.is_none() {
+            return Err(StudyListError::ListNotFound);
+        }
 
         if let Some(current) = &self.config.current {
-            println!("{} {}", current, list_name);
             if current == list_name {
                 self.config.current = None;
             }
         }
 
-        self.save()
-    }
-
-    fn load(filepath: &PathBuf) -> Result<StudyListConfig, Error> {
-        if !&filepath.exists() {
-            let json = serde_json::to_string::<StudyListConfig>(&StudyListConfig::default())?;
-            fs::write(filepath, json)?;
-        }
-
-        let file = fs::read_to_string(filepath)?;
-        let config = serde_json::from_str::<StudyListConfig>(&file)?;
-
-        Ok(config)
-    }
-
-    fn save(&self) -> Result<(), Error> {
-        let json = serde_json::to_string::<StudyListConfig>(&self.config)?;
-        fs::write(&self.filepath, json)?;
-
-        Ok(())
+        save(&self.filepath, &self.config)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashSet, path::PathBuf};
+    use std::{collections::HashSet, fs, path::PathBuf};
 
     use tempfile::TempDir;
     use test_case::test_case;
 
-    use crate::core::models::study_list::StudyConfig;
+    use crate::core::{
+        models::study_list::{StudyConfig, StudyListConfig},
+        study_list_stats::{load, save, StudyListError},
+    };
 
     use super::StudyListStats;
 
     fn setup() -> (PathBuf, StudyListStats) {
         let tmp_dir: TempDir = TempDir::new().unwrap();
-        let filepath = tmp_dir.into_path().join("stats.json");
+        let tmp_path = tmp_dir.into_path();
 
-        let study_list_stats = StudyListStats::new(filepath.clone());
+        let filepath = tmp_path.join("stats.json");
+        let study_list_stats = StudyListStats::new(filepath.clone()).unwrap();
 
-        (filepath, study_list_stats)
+        (tmp_path, study_list_stats)
     }
 
     #[test]
     fn test_creation() {
-        let (filepath, study_list_stats) = setup();
+        let (_, study_list_stats) = setup();
 
-        assert!(&filepath.exists());
+        assert!(study_list_stats.filepath.exists());
 
-        assert_eq!(study_list_stats.config.current, None);
+        assert!(study_list_stats.config.current.is_none());
         assert_eq!(study_list_stats.config.lists.len(), 0);
     }
 
     #[test]
     fn test_invalid_list() {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         assert!(study_list_stats.get_list("invalid_list").is_none());
         assert!(study_list_stats.select_list("invalid_list").is_err());
         assert!(study_list_stats.remove_stats("invalid_list").is_err());
 
-        assert_eq!(study_list_stats.config.current, None);
+        assert!(study_list_stats.config.current.is_none());
         assert_eq!(study_list_stats.config.lists.len(), 0);
     }
 
     #[test]
     fn test_select_list() {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         let config = StudyConfig::default();
         study_list_stats.update_stats("valid_name", config).unwrap();
@@ -132,7 +150,7 @@ mod tests {
     #[test_case("list_2", StudyConfig { current_index: 0, items_per_day: 0 })]
     #[test_case("list_3", StudyConfig { current_index: 42, items_per_day: 5 })]
     fn test_get_list_valid(name: &str, config: StudyConfig) {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         study_list_stats.update_stats(name, config).unwrap();
         assert_eq!(study_list_stats.get_list(name).unwrap(), &config);
@@ -143,7 +161,7 @@ mod tests {
     #[test_case(vec!["list_1", "list_2"])]
     #[test_case(vec!["list_1", "list_2", "list_3"])]
     fn test_get_lists(items: Vec<&str>) {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         let config = StudyConfig::default();
 
@@ -169,7 +187,7 @@ mod tests {
 
     #[test]
     fn test_remove_list() {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         let config = StudyConfig::default();
         study_list_stats.update_stats("list_1", config).unwrap();
@@ -189,7 +207,7 @@ mod tests {
 
     #[test]
     fn test_remove_selected_list() {
-        let (_filepath, mut study_list_stats) = setup();
+        let (_, mut study_list_stats) = setup();
 
         let config = StudyConfig::default();
         study_list_stats.update_stats("list_1", config).unwrap();
@@ -204,5 +222,24 @@ mod tests {
         assert!(study_list_stats.get_list("list_1").is_none());
         assert!(study_list_stats.select_list("list_1").is_err());
         assert!(study_list_stats.config.current.is_none());
+    }
+
+    #[test]
+    fn test_file_not_found() {
+        let (tmp_dir, study_list_stats) = setup();
+        fs::remove_dir_all(tmp_dir).unwrap();
+
+        let filepath = &study_list_stats.filepath;
+        let config = StudyListConfig::default();
+
+        assert!(matches!(
+            load(filepath).err().unwrap(),
+            StudyListError::Io(_)
+        ));
+
+        assert!(matches!(
+            save(filepath, &config).err().unwrap(),
+            StudyListError::Io(_)
+        ));
     }
 }
