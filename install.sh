a5f105fd178af3fc89bd12115a3940ee31caf785
saiteki-kai <giuseppe.magazu@gmail.com>
2024-04-17 18:11:29 +0200

    chore: install packages for matching script

--- install.sh
+++ install.sh
@@ -8,7 +8,11 @@ fi
 
 INSTALL_DIR=$LOCAL_INSTALL_DIR
 
-cp matching/dist/extract_words $INSTALL_DIR
+# Install matching script
+cd matching/
+pip install -r requirements.txt && ./install.sh 
+cp dist/extract_words $INSTALL_DIR
+cd ../
 
 # Build project
 cargo build --release
