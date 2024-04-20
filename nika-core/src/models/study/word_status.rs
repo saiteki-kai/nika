45bb7e7bac832a24d0d5604858ab78bdad36cb4a
saiteki-kai <giuseppe.magazu@gmail.com>
2024-04-20 20:18:16 +0200

    test: add unit test for status conversions

--- nika-core/src/models/study/word_status.rs
+++ nika-core/src/models/study/word_status.rs
@@ -19,7 +19,7 @@ impl From<&Status> for String {
             Status::Skipped => "skipped".into(),
             Status::Discarded => "discarded".into(),
             Status::Done => "done".into(),
-            Status::New => "todo".into(),
+            Status::New => "new".into(),
         }
     }
 }
@@ -30,7 +30,7 @@ impl From<&str> for Status {
             "skipped" => Status::Skipped,
             "discarded" => Status::Discarded,
             "done" => Status::Done,
-            "todo" => Status::New,
+            "new" => Status::New,
             _ => Status::New,
         }
     }
@@ -42,7 +42,7 @@ impl From<&Status> for &[u8] {
             Status::Skipped => b"skipped",
             Status::Discarded => b"discarded",
             Status::Done => b"done",
-            Status::New => b"todo",
+            Status::New => b"new",
         }
     }
 }
@@ -57,5 +57,57 @@ impl ToSql for Status {
 impl FromSql for Status {
     fn column_result(value: ValueRef<'_>) -> FromSqlResult<Self> {
         Ok(Status::from(value.as_str()?))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case("skipped", Status::Skipped)]
+    #[test_case("discarded", Status::Discarded)]
+    #[test_case("done", Status::Done)]
+    #[test_case("new", Status::New)]
+    fn test_from_str_valid(value: &str, status: Status) {
+        assert_eq!(Status::from(value), status);
+    }
+
+    #[test_case("invalid")]
+    #[test_case("")]
+    fn test_from_str_invalid(value: &str) {
+        assert_eq!(Status::from(value), Status::New);
+    }
+
+    #[test_case(Status::Skipped, "skipped".to_owned())]
+    #[test_case(Status::Discarded, "discarded".to_owned())]
+    #[test_case(Status::Done, "done".to_owned())]
+    #[test_case(Status::New, "new".to_owned())]
+    fn test_from_status(status: Status, value: String) {
+        assert_eq!(String::from(&status), value);
+    }
+
+    #[test_case(Status::Skipped, b"skipped")]
+    #[test_case(Status::Discarded, b"discarded")]
+    #[test_case(Status::Done, b"done")]
+    #[test_case(Status::New, b"new")]
+    fn test_to_sql(status: Status, value: &[u8]) {
+        assert_eq!(
+            status.to_sql(),
+            Ok(ToSqlOutput::Borrowed(ValueRef::Text(value)))
+        );
+    }
+
+    #[test_case(b"skipped", Status::Skipped)]
+    fn test_from_sql(value: &[u8], status: Status) {
+        let value = ValueRef::Text(value);
+        assert_eq!(Status::column_result(value), Ok(status));
+    }
+
+    #[test]
+    fn test_from_sql_invalid() {
+        let value = ValueRef::Text(b"invalid");
+        assert_eq!(Status::column_result(value), Ok(Status::New));
     }
 }
